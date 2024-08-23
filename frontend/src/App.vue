a6a6b96bf40fd1c2d5942357c5d8acc7b792ff04
JsonSept <jasonseptember.za@gmail.com>
2024-08-23 14:51:31 +0200

    update

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <navbar-comp/>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<script>
+import NavbarComp from './components/NavbarComp.vue'
+export default {
+  components: {
+      NavbarComp
+    }
+  }
+
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>dbahkdvhbaldisbvaljsbdvljbsdljv</p>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      message: '',
+    };
+  },
+  mounted() {
+    axios.get('/')
+      .then(response => {
+        this.message = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+}
+</script>
